9e06fd6c4f6c132e8efea44f31692ae0dbf789b2
ledfyr <david.ramstrom@gmail.com>
2019-11-21 21:42:48 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,175 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+const fs = require('fs');
+const archiver = require('archiver');
+const extract = require('extract-zip')
+//const logger = require('morgan');
+
+const soundFileExtension = '.wav';
+const collator = new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' });
+const rootDir = '/home/pi/samples';
+const newDirName = 'newdir';
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+let currentDir = rootDir;
+
+app.use(cors());
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit:'5gb' }));
+//app.use(logger('dev'));
+
+list = () => {
+  return fs.readdirSync(currentDir).sort(collator.compare);
+}
+
+listDirs = () => {
+  return fs.readdirSync(currentDir)
+           .filter(f => fs.statSync(path.join(currentDir, f)).isDirectory());
+}
+
+rename = (from, to) => {
+  fs.renameSync(
+    path.join(currentDir, from),
+    path.join(currentDir, to)
+  );
+}
+
+remove = (file) => {
+  if (fs.statSync(path.join(currentDir, file)).isDirectory()) {
+    fs.rmdirSync(path.join(currentDir, file), { recursive: true });
+  }
+  else {
+    fs.unlinkSync(path.join(currentDir, file));
+  }
+}
+
+router.get('/data', (req, res) => {
+  let files = {};
+  list().forEach(file => {
+    files[file] = '';
+  });
+  const dirs = listDirs();
+  res.json({ success: true, files: files, dirs: dirs, currentDir: currentDir });
+});
+
+router.post('/cwd', (req, res) => {
+  currentDir = path.join(currentDir, req.body.dir);
+  res.json({ success: true });
+});
+
+router.delete('/cwd', (req, res) => {
+  currentDir = rootDir;
+  res.json({ success: true });
+});
+
+router.post('/rename', (req, res) => {
+  const files = req.body;
+  for (let key in files) {
+    if (files[key].startsWith('---')) {
+      remove(key);
+    }
+    else {
+      rename(key, files[key]);
+    }
+  }
+  res.json({ success: true });
+});
+
+router.post('/format', (req, res) => {
+  const indexRegex = /^[0-9]+_(.+)/;
+  const volumeRegex = /_[0-9]+(d[0-9]+|$)/;
+  const files = list();
+  let i = Number(req.body.index);
+  files.forEach(file => {
+    let extension = path.extname(file);
+    if (extension.toLowerCase() != soundFileExtension) {
+      return;
+    }
+    let name = path.basename(file, extension);
+    const matches = name.match(indexRegex);
+    if (matches && matches.length === 2) {
+      name = matches[1];
+    }
+    let volume = '';
+    if (!volumeRegex.test(name)) {
+      volume = '_100';
+    }
+    const newName = i + '_' + name + volume + extension.toLowerCase();
+    if (file !== newName) {
+      rename(file, newName);
+    }
+    i++;
+  });
+  res.json({ success: true });
+});
+
+router.post('/unzip', (req, res) => {
+  extract(path.join(currentDir, req.body.file), { dir: currentDir }, err => {
+    if (err) {
+      res.status(500).json({ error: err });
+    }
+    else {
+      res.json({ success: true });
+    }
+  });
+});
+
+router.post('/zip', (req, res) => {
+  const archive = archiver('zip');
+  const file = req.body.file;
+  const output = fs.createWriteStream(path.join(currentDir, file + '.zip'));
+
+  output.on('close', () => {
+    res.json({ success: true });
+  });
+  archive.on('warning', err => {
+    res.status(500).json({ error: err.message });
+  });
+  archive.on('error', err => {
+    res.status(500).json({ error: err.message });
+  });
+  archive.pipe(output);
+
+  const filePath = path.join(currentDir, file);
+  if (fs.statSync(filePath).isDirectory()) {
+    archive.directory(filePath, file);
+  }
+  else {
+    archive.file(filePath, { name: file });
+  }
+  archive.finalize();
+});
+
+router.post('/upload', (req, res, next) => {
+  const files = req.files;
+  for (key in files) {
+    const file = files[key];
+    file.mv(path.join(currentDir, file.name));
+  }
+  res.json({ success: true });
+});
+
+router.get('/download/:filename', (req, res) => {
+  res.download(path.join(currentDir, req.params.filename));
+});
+
+router.post('/mkdir', (req, res) => {
+  fs.mkdirSync(path.join(currentDir, newDirName), { recursive: false }, err => {
+    if (err) {
+      res.status(500).json({ error: err });
+    }
+  });
+  res.json({ success: true });
+});
+
+router.get('/kill', (req, res) => {
+  process.exit(0);
+});
+
+app.use('/api', router);
+app.listen(API_PORT);
---
+++ frontend/src/App.js
@@ -0,0 +1,415 @@
+import React, { Component } from 'react';
+import update from 'react-addons-update';
+import {Form, Row, Col, Button, Dropdown, Container} from 'react-bootstrap';
+import {Range} from 'react-range';
+
+class App extends Component {
+  state = {
+    files: {},
+    id: 0,
+    message: null,
+    currentDir: '',
+    dirs: [],
+    values: [50],
+    upload: null,
+    uploadInput: null,
+  };
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    this.clearUpload();
+    fetch('http://localhost:3001/api/data')
+      .then(data => data.json())
+      .then(res => this.setState({
+        files: res.files,
+        currentDir: res.currentDir,
+        dirs: res.dirs
+      }));
+  }
+
+  handleChange = (event) => {
+    const newState = update(this.state, {
+      files: {
+        [event.target.id]: {$set: event.target.value}
+      }
+    });
+    this.setState(newState);
+  }
+
+  handleFormat = (eventKey, event) => {
+    event.preventDefault();
+    const body = { index: eventKey };
+    fetch('http://localhost:3001/api/format', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(data => data.json())
+    .then(res => {
+      this.fetchData();
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let filtered = {};
+    for (let [key, value] of Object.entries(this.state.files)) {
+      if (value) {
+        filtered[key] = value;
+      }
+    }
+    this.setState({ files: {} });
+    fetch('http://localhost:3001/api/rename', {
+      method: 'POST',
+      body: JSON.stringify(filtered),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(data => data.json())
+    .then(res => {
+      this.fetchData();
+    });
+  }
+
+  handleChangeDir = (eventKey, event) => {
+    event.preventDefault();
+    const body = { dir: eventKey };
+    fetch('http://localhost:3001/api/cwd', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(data => data.json())
+    .then(res => {
+      this.fetchData();
+    });
+  }
+
+  handleChangeDirRoot = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:3001/api/cwd', {
+      method: 'DELETE',
+      body: '{}',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(data => data.json())
+    .then(res => {
+      this.fetchData();
+    });
+  }
+
+  handleCancel = (event) => {
+    event.preventDefault();
+    this.setState({ files: {} });
+    this.fetchData();
+  }
+
+  handleDelete = (key) => {
+    const { files } = this.state;
+    const filename = files[key] ? files[key] : key;
+    const newValue = '---' + filename;
+    const newState = update(this.state, {
+      files: {
+        [key]: {$set: newValue}
+      }
+    });
+    this.setState(newState);
+  }
+
+  handleZip = (key) => {
+    const body = { file: key };
+    fetch('http://localhost:3001/api/zip', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(data => data.json())
+    .then(res => {
+      this.fetchData();
+    });
+  }
+
+  handleUnzip = (key) => {
+    const body = { file: key };
+    fetch('http://localhost:3001/api/unzip', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(data => data.json())
+    .then(res => {
+      this.fetchData();
+    });
+  }
+
+  handleNewDirectory = () => {
+    fetch('http://localhost:3001/api/mkdir', {
+      method: 'POST',
+      body: '{}',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(data => data.json())
+    .then(res => {
+      this.fetchData();
+    });
+  }
+
+  handleUpload = (event) => {
+    event.preventDefault();
+    const { upload } = this.state;
+    const filedata = new FormData();
+    upload.forEach(file => {
+      filedata.append(file.name, file);
+    });
+
+    fetch('http://localhost:3001/api/upload', {
+      method: 'POST',
+      body: filedata,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(data => data.json())
+    .then(res => {
+      this.fetchData();
+    });
+    this.clearUpload();
+  }
+
+  clearUpload = () => {
+    const { uploadInput } = this.state;
+    if (uploadInput !== null) {
+      uploadInput.value = null;
+    }
+    this.setState({ upload: null });
+  }
+
+  addFiles = (event) => {
+    this.setState({ uploadInput: event.target });
+    this.setState({ upload: Array.from(event.target.files) });
+  }
+
+  getVolume = (key) => {
+    const { files } = this.state;
+    const filename = files[key] ? files[key] : key;
+    const volumeRegex = /_([0-9]+)(d[0-9]+.wav|.wav$)/;
+    const matches = filename.match(volumeRegex);
+    if (matches && matches.length === 3) {
+      return [Number(matches[1])];
+    }
+    return [1];
+  }
+
+  setVolume = (key, volume) => {
+    const { files } = this.state;
+    const filename = files[key] ? files[key] : key;
+    const volumeRegex = /(.*_)([0-9]+)(d[0-9]+.wav|.wav$)/;
+    const matches = filename.match(volumeRegex);
+    if (matches && matches.length === 4) {
+      const newFilename = matches[1] + volume[0] + matches[3];
+      const newState = update(this.state, {
+        files: {
+          [key]: {$set: newFilename}
+        }
+      });
+      this.setState(newState);
+    }
+  }
+
+  getVolumeTrackStyle = (key, dirs, props) => {
+    if (key.match(/wav$/)) {
+      return {
+        ...props.style,
+        marginTop: '12px',
+        height: '16px',
+        backgroundColor: '#9cc'
+      };
+    }
+    else if (dirs.includes(key)) {
+      return {
+        ...props.style,
+        marginTop: '12px',
+        height: '16px',
+        backgroundColor: '#fda'
+      };
+    }
+    else {
+      return {
+        ...props.style,
+        marginTop: '12px',
+        height: '16px',
+        backgroundColor: '#ccc'
+      };
+    }
+  }
+
+  getVolumeThumbStyle = (key, dirs, props) => {
+    if (key.match(/wav$/)) {
+      return {
+        ...props.style,
+        height: "24px",
+        width: "16px",
+        backgroundColor: "#777"
+      };
+    }
+    else if (dirs.includes(key)) {
+      return {
+        ...props.style,
+        height: "16px",
+        width: "1px",
+        backgroundColor: "#fda"
+      };
+    }
+    else {
+      return {
+        ...props.style,
+        height: "16px",
+        width: "1px",
+        backgroundColor: "#ccc"
+      };
+    }
+  }
+
+  getFilenameStyle = (key, files) => {
+    const file = files[key];
+    if (file.startsWith("---")) {
+      return {
+        color: "#f00",
+        fontWeight: "bold"
+      };
+    }
+    else if (file && key !== file) {
+      return {
+        color: "#0b0",
+        fontStyle: "italic",
+        fontWeight: "bold",
+        textDecoration: "underline"
+      };
+    }
+    else {
+      return {};
+    }
+  }
+
+  render() {
+    const { files, currentDir, dirs, upload } = this.state;
+    const flexStyle = {
+      display: "flex",
+      flexWrap: "wrap"
+    };
+
+    return (
+      <div>
+        <h1>SamplerFox</h1>
+        <h4>Navigation</h4>
+        <div style={flexStyle}>
+          <Button variant="secondary" onClick={this.handleChangeDirRoot}>
+            Home
+          </Button>
+          <Dropdown onSelect={this.handleChangeDir}>
+            <Dropdown.Toggle variant="warning" id="dropdown-basic">
+              {currentDir}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {dirs.length === 0 ?
+                 <Dropdown.Item key="none" disabled="true">None</Dropdown.Item>
+               : dirs.map(dir =>
+                 <Dropdown.Item key={dir} eventKey={dir}>{dir}</Dropdown.Item>
+               )}
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+        <h4 style={{paddingTop: "12px"}}>Upload Files</h4>
+        <Form onSubmit={this.handleUpload}>
+        <div style={flexStyle}>
+          <Button style={{height: "38px"}} variant="primary" type="submit" disabled={upload === null}>Upload</Button>
+          <Form.Group>
+            <Form.Control style={{paddingLeft: "2px", paddingTop: "4px"}} id="fileUpload" type="file" multiple onChange={this.addFiles}/>
+          </Form.Group>
+        </div>
+        </Form>
+        <h4>File List</h4>
+        <Form onSubmit={this.handleSubmit}>
+          <div style={flexStyle}>
+            <Button variant="secondary" onClick={this.handleCancel}>
+              Cancel
+            </Button>
+            <Button variant="warning" disabled={Object.keys(files).includes('newdir')} onClick={this.handleNewDirectory}>
+              New Directory
+            </Button>
+            <Dropdown onSelect={this.handleFormat}>
+              <Dropdown.Toggle variant="info" id="dropdown-basic">
+                Format
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item eventKey="0">0</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </div>
+          {Object.keys(files).map(key => 
+            <Container key={key} fluid={true}>
+              <Form.Group as={Row} key={key}>
+                <Col sm="4">
+                <Range
+                  step={1}
+                  min={0}
+                  max={100}
+                  disabled={!key.match(/wav$/)}
+                  values={this.getVolume(key)}
+                  onChange={val => this.setVolume(key, val)}
+                  renderTrack={({ props, children }) => (
+                    <div {...props} style={this.getVolumeTrackStyle(key, dirs, props)}>
+                      {children}
+                    </div>
+                  )}
+                  renderThumb={({ props }) => (
+                    <div {...props} style={this.getVolumeThumbStyle(key, dirs, props)}/>
+                  )}
+                />
+                </Col>
+                <Col sm="6">
+                  <Form.Control id={key} style={this.getFilenameStyle(key, files)} plaintext value={files[key] ? files[key] : key} onChange={this.handleChange}/>
+                </Col>
+                <Col sm="2">
+                  <Button variant="success" disabled={dirs.includes(key)} href={"http://localhost:3001/api/download/" + key}>
+                    v
+                  </Button>
+                  <Button variant="primary" disabled={key.endsWith(".zip")} onClick={() => this.handleZip(key)}>
+                    z
+                  </Button>
+                  <Button variant="secondary" disabled={!key.endsWith(".zip") || Object.keys(files).includes(key.replace(/\.zip$/, ""))} onClick={() => this.handleUnzip(key)}>
+                    u
+                  </Button>
+                  <Button variant="danger" disabled={files[key].startsWith("---")} onClick={() => this.handleDelete(key)}>
+                    -
+                  </Button>
+                </Col>
+              </Form.Group>
+            </Container>
+          )}
+        </Form>
+      </div>
+    );
+  }
+}
+export default App;
